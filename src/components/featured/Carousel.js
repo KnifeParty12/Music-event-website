5975046fa691f914f9f0496f701f95336b17ba05
KnifeParty12 <flashfunk01@gmail.com>
2019-03-09 02:55:48 +0530

    Finalized Timer

--- src/components/featured/Carousel.js
+++ src/components/featured/Carousel.js
@@ -4,6 +4,7 @@ import Slider from 'react-slick';
 import slide_one from '../../resources/images/slide_one.jpg';
 import slide_two from '../../resources/images/slide_two.jpg';
 import slide_three from '../../resources/images/slide_three.jpg';
+
 const Carousel = () => {
 
     const settings = {
@@ -26,7 +27,7 @@ const Carousel = () => {
             <div className={"carousel_image"}
             style={{
                 background: `url(${slide_one})`,
-                height: `${window.innerHeight}px`
+                height: `${window.innerHeight}px`,
             }}
             >
 
--- src/components/featured/TimeUntil.js
+++ src/components/featured/TimeUntil.js
@@ -1,49 +1,78 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Slide from 'react-reveal/Slide';
+
 class TimeUntil extends Component {
 
-    state ={
+    state = {
+        deadline: 'Mar,16,2019',
+        days:'0',
+        hours:'0',
+        minutes:'0',
+        seconds:'0'
+    }
 
-    };
+    getTimeUntil(deadline){
+        const time = Date.parse(deadline) - Date.parse(new Date());
+        if(time < 0) {
+            console.log('Date passed')
+        } else {
+            const seconds = Math.floor((time/1000)%60);
+            const minutes = Math.floor((time/1000/60)%60);
+            const hours = Math.floor((time/(1000*60*60))%24);
+            const days = Math.floor(time/(1000*60*60*24));
+
+            this.setState({
+                days,
+                hours,
+                minutes,
+                seconds
+            })
+        }
+
+    }
+
+    componentDidMount(){
+        setInterval(()=> this.getTimeUntil(this.state.deadline),1000)
+    }
 
 
     render() {
         return (
             <Slide left delay={1000}>
-                <div className={'countdown_wrapper'}>
-                    <div className={'countdown_top'}>
-                        Event Starts in
+                <div className="countdown_wrapper">
+                    <div className="countdown_top">
+                        Event starts in
                     </div>
-                    <div className={'countdown_bottom'}>
-                        <div className={'countdown_item'}>
-                            <div className={'countdown_time'}>
-                                23
+                    <div className="countdown_bottom">
+                        <div className="countdown_item">
+                            <div className="countdown_time">
+                                {this.state.days}
                             </div>
-                            <div className={'countdown_tag'}>
+                            <div className="countdown_tag">
                                 Days
                             </div>
                         </div>
-                        <div className={'countdown_item'}>
-                            <div className={'countdown_time'}>
-                                2
+                        <div className="countdown_item">
+                            <div className="countdown_time">
+                                {this.state.hours}
                             </div>
-                            <div className={'countdown_tag'}>
+                            <div className="countdown_tag">
                                 Hs
                             </div>
                         </div>
-                        <div className={'countdown_item'}>
-                            <div className={'countdown_time'}>
-                                20
+                        <div className="countdown_item">
+                            <div className="countdown_time">
+                                {this.state.minutes}
                             </div>
-                            <div className={'countdown_tag'}>
+                            <div className="countdown_tag">
                                 Min
                             </div>
                         </div>
-                        <div className={'countdown_item'}>
-                            <div className={'countdown_time'}>
-                                40
+                        <div className="countdown_item">
+                            <div className="countdown_time">
+                                {this.state.seconds}
                             </div>
-                            <div className={'countdown_tag'}>
+                            <div className="countdown_tag">
                                 Sec
                             </div>
                         </div>
