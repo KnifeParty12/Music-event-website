6ff32b9c9e69bf806002eaf4aad4b0c7a4746580
KnifeParty12 <flashfunk01@gmail.com>
2019-03-09 00:01:42 +0530

    finalized header animation

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './components/header-footer/Header';
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{height: "1500px",background: 'red'}}>
+      <div className="App" style={{height: "1500px",background: 'lightblue'}}>
         <Header/>
       </div>
     );
--- src/components/header-footer/Header.js
+++ src/components/header-footer/Header.js
@@ -10,7 +10,26 @@ import SideDrawer from './SideDrawer';
 class Header extends Component {
 
     state = {
-        drawerOpen: false
+        drawerOpen: false,
+        headerShow: false
+    };
+
+    //Attach an eventlistener for scroll event
+
+    componentDidMount() {
+        window.addEventListener('scroll',this.handleScroll);
+    }
+
+    handleScroll = () => {
+    if (window.scrollY > 0){
+        this.setState({
+            headerShow: true
+        })
+    } else {
+        this.setState({
+            headerShow: false
+        })
+    }
     };
 
     toggleDrawer = (value) => {
@@ -25,7 +44,7 @@ class Header extends Component {
         <AppBar
             position={"fixed"}
             style={{
-                backgroundColor: '#2f2f2f',
+                backgroundColor:this.state.headerShow ? '#2f2f2f' : 'transparent',
                 boxShadow: 'none',
                 padding: '10px,0px'
             }}
