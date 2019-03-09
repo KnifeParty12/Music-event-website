1ea6fa323170d97bda328376e98e85635f9acdc9
KnifeParty12 <flashfunk01@gmail.com>
2019-03-09 21:15:19 +0530

    Reusable Purchase Button

--- src/components/Highlights/Discount.js
+++ src/components/Highlights/Discount.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Fade from 'react-reveal/Fade';
 import Slide from 'react-reveal/Slide';
-
+import MyButton from '../utils/MyButton';
 
 class Discount extends Component {
 
@@ -46,15 +46,18 @@ class Discount extends Component {
                     <Slide right>
 
                         <div className={'discount_description'}>
-                            <h3>Purchase tickets before 16th MARCH</h3>
+                            <h3>Purchase tickets before <b>16th MARCH</b></h3>
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                                 when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
                                 and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                             </p>
 
-                            <div>
-                                Button
-                            </div>
+                        <MyButton
+                            text = 'Purchase tickets'
+                            bck={'#ffa800'}
+                            color={'#ffffff'}
+                            link={'https://www.google.co.in/'}
+                        />
 
                         </div>
 
