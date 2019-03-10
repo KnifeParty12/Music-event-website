37bd399f21d500bd1d7c3335c83edd133f75d4d3
KnifeParty12 <flashfunk01@gmail.com>
2019-03-10 20:24:22 +0530

    Finalizing and Deploying On Surge

--- src/components/header-footer/Header.js
+++ src/components/header-footer/Header.js
@@ -51,7 +51,7 @@ class Header extends Component {
         >
             <Toolbar>
             <div className={"header_logo"}>
-            <div className={'font_righteous header_logo_venue'}>The Venue </div>
+            <div className={'font_righteous header_logo_venue'}>EDM Upstage </div>
             <div className={'header_logo_title'}>Musical Events</div>
             </div>
 
